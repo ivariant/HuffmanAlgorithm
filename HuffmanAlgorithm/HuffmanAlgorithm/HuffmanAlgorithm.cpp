2f1bb5129294f72996b9d0a28624f33c96b64804
iVariant <ix.variant@gmail.com>
2017-05-28 18:10:35 +0300

    Create

---
+++ HuffmanAlgorithm/HuffmanAlgorithm/HuffmanAlgorithm.cpp
@@ -0,0 +1,33 @@
+// HuffmanAlgorithm.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+void pack(std::string);
+
+
+int main()
+{
+	std::string str = "Hello world!!!!";
+
+	pack(str);
+
+
+	std::cin >> str;
+	return 0;
+}
+
+
+void pack(std::string str)
+{
+
+	std::map<std::string, int> contChar;
+
+	for (int i(0); i < str.length(); i++)
+	{
+		//if(contChar.f)
+	}
+
+
+}
